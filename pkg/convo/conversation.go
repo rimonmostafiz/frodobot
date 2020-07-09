381b4cc56b1ea6cb7010152f849b0d566b56b0aa
Rimon Mostafiz <rimonmostafiz@gmail.com>
2020-07-09 23:57:12 +0600

    Add latest time param on conversation history parameter :zap:

--- pkg/convo/conversation.go
+++ pkg/convo/conversation.go
@@ -8,24 +8,31 @@ import (
 	"time"
 )
 
-// Values for oldest conversion time calculation
+// Values for conversation starting and ending time calculation
 var (
-	year, month, day     = time.Now().Date()
-	hour, min, sec, nsec = 06, 00, 00, 000
-	today                = time.Date(year, month, day, hour, min, sec, nsec, time.Now().Location())
-	todayUnix            = strconv.FormatInt(today.Unix(), 10)
+	year, month, day = time.Now().Date()
+	startHour        = 06
+	startMin         = 00
+	startSec         = 0
+	startingTime     = time.Date(year, month, day, startHour, startMin, startSec, 000, time.Now().Location())
+	startingTimeUnix = strconv.FormatInt(startingTime.Unix(), 10)
+	endHour          = 10
+	endMin           = 45
+	endSec           = 00
+	endTime          = time.Date(year, month, day, endHour, endMin, endSec, 000, time.Now().Location())
+	endTimeUnix      = strconv.FormatInt(endTime.Unix(), 10)
 )
 
-// GetConversationsHistory returns slice of slack.Message for a given channel
+// GetConversationsHistory returns slice of slack.Message for a given channel from oldest time to latest time
 // As a param we send channelId and Oldest time of the message to consider
 func GetConversationsHistory(channelId string, client *slack.Client) []slack.Message {
 	historyParam := slack.GetConversationHistoryParameters{
 		ChannelID: channelId,
 		Cursor:    "",
 		Inclusive: false,
-		Latest:    "",
+		Latest:    endTimeUnix,
 		Limit:     0,
-		Oldest:    todayUnix,
+		Oldest:    startingTimeUnix,
 	}
 
 	history, err := client.GetConversationHistory(&historyParam)
