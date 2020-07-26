39de448b5141e7c5e07fb85ca3dbfe3b893ae0c5
Rimon Mostafiz <rimonmostafiz@gmail.com>
2020-07-27 05:54:24 +0600

    Introduce scheduling with corn, Application will send reminder to slack channel on every day-of-week at 10:45AM from Sunday to Tuesday, Add logging :zap:

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/mitchellh/mapstructure v1.3.2 // indirect
 	github.com/pelletier/go-toml v1.8.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/robfig/cron/v3 v3.0.0
 	github.com/slack-go/slack v0.6.5
 	github.com/spf13/afero v1.3.1 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
--- main.go
+++ main.go
@@ -5,29 +5,61 @@ import (
 	"github.com/rimonmsotafiz/flott-bot/pkg/cfg"
 	"github.com/rimonmsotafiz/flott-bot/pkg/convo"
 	"github.com/rimonmsotafiz/flott-bot/pkg/user"
+	"github.com/robfig/cron/v3"
 	"github.com/slack-go/slack"
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+var (
+	InfoLogger *log.Logger
+	ErrLogger  *log.Logger
 )
 
 func main() {
+	initLogger()
+	InfoLogger.Printf("Strat Flott Bot Application")
+	c := cron.New()
 	cfg.InitViper(".env")
 
 	token := cfg.ReadFromEnv("SLACK_TOKEN")
 	channelId := cfg.ReadFromEnv("CHANNEL_ID")
-	testChannelId := cfg.ReadFromEnv("TEST_CHANNEL_ID")
+	//testChannelId := cfg.ReadFromEnv("TEST_CHANNEL_ID")
 	excludeUserMap := cfg.ReadStringMapFromEnv("EXCLUDE_USER_VALUES")
 	slackClient := slack.New(token)
 
-	users := user.GetUserList(channelId, slackClient)
-	userMap := user.InitUserMap(users, excludeUserMap)
-	messages := convo.GetConversationsHistory(channelId, slackClient)
-	user.MarkUserAsGreen(messages, userMap)
-	usernames, count := user.GetUserToRemind(userMap, slackClient)
+	_, _ = c.AddFunc("45 10 * * 0-4", func() {
+		now := time.Now()
+		sec := now.Unix()
+		nSec := now.UnixNano()
+		InfoLogger.Printf("Cron Started at %v", time.Unix(sec, nSec))
+		users := user.GetUserList(channelId, slackClient)
+		userMap := user.InitUserMap(users, excludeUserMap)
+		messages := convo.GetConversationsHistory(channelId, slackClient)
+		user.MarkUserAsGreen(messages, userMap)
+		usernames, count := user.GetUserToRemind(userMap, slackClient)
 
-	fmt.Println("User to remind:", count)
+		fmt.Printf("Total %v User to remind\n", count)
 
-	if count > 0 {
-		msg := convo.ConstructReminderMsg(usernames)
-		fmt.Println(msg)
-		convo.SendReminder(testChannelId, msg, slackClient)
+		if count > 0 {
+			msg := convo.ConstructReminderMsg(usernames)
+			InfoLogger.Println(msg)
+			convo.SendReminder(channelId, msg, slackClient)
+		}
+	})
+
+	c.Start()
+	<-make(chan struct{})
+}
+
+func initLogger() {
+	file, err := os.OpenFile("flott-bot.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
 	}
+	mw := io.MultiWriter(os.Stdout, file)
+	InfoLogger = log.New(mw, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrLogger = log.New(mw, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
