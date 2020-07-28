cefb0760608014c24ab5f972a470903f5a91182a
Rimon Mostafiz <rimonmostafiz@gmail.com>
2020-07-28 12:24:51 +0600

    Remove start and end time calculation of conversation history from global scope :bug:

--- pkg/convo/conversation.go
+++ pkg/convo/conversation.go
@@ -8,31 +8,29 @@ import (
 	"time"
 )
 
-// Values for conversation starting and ending time calculation
-var (
-	year, month, day = time.Now().Date()
-	startHour        = 06
-	startMin         = 00
-	startSec         = 0
-	startingTime     = time.Date(year, month, day, startHour, startMin, startSec, 000, time.Now().Location())
-	startingTimeUnix = strconv.FormatInt(startingTime.Unix(), 10)
-	endHour          = 10
-	endMin           = 45
-	endSec           = 00
-	endTime          = time.Date(year, month, day, endHour, endMin, endSec, 000, time.Now().Location())
-	endTimeUnix      = strconv.FormatInt(endTime.Unix(), 10)
-)
+// getConversationWindow will calculate and returns the time window(startTime and endTime) for conversation history
+func getConversationWindow() (string, string) {
+	year, month, day := time.Now().Date()
+	startHour, startMin, startSec := 06, 00, 00
+	startingTime := time.Date(year, month, day, startHour, startMin, startSec, 000, time.Now().Location())
+	startingTimeUnix := strconv.FormatInt(startingTime.Unix(), 10)
+	endHour, endMin, endSec := 10, 45, 00
+	endTime := time.Date(year, month, day, endHour, endMin, endSec, 000, time.Now().Location())
+	endTimeUnix := strconv.FormatInt(endTime.Unix(), 10)
+	return startingTimeUnix, endTimeUnix
+}
 
 // GetConversationsHistory returns slice of slack.Message for a given channel from oldest time to latest time
 // As a param we send channelId and Oldest time of the message to consider
 func GetConversationsHistory(channelId string, client *slack.Client) []slack.Message {
+	startTime, endTime := getConversationWindow()
 	historyParam := slack.GetConversationHistoryParameters{
 		ChannelID: channelId,
 		Cursor:    "",
 		Inclusive: false,
-		Latest:    endTimeUnix,
+		Latest:    endTime,
 		Limit:     0,
-		Oldest:    startingTimeUnix,
+		Oldest:    startTime,
 	}
 
 	history, err := client.GetConversationHistory(&historyParam)
